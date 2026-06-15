409395472e0ce41abeb4be42695644b94660a5a0
Roland <roland.khater89@gmail.com>
2026-06-15 21:06:49 +0300

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import NoProject from "./Components/NoProjects";
 import SideBar from "./Components/SideBar";
+import NewProject from "./Components/NewProject";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <SideBar />
-      <NoProject />
+      <NewProject />
+      {/* <NoProject /> */}
     </main>
   );
 }
