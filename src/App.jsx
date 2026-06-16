881eb16e94a449151df07f970951878d71f9b50a
Roland Khater <roland.khater89@gmail.com>
2026-06-16 16:32:32 +0300

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
 import NoProject from "./Components/NoProjects";
 import SideBar from "./Components/SideBar";
 import NewProject from "./Components/NewProject";
+import { useState } from "react";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId:undefined,
+    projects:[]
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prev =>{
+      return{...prev,
+      selectedProjectId:null,
+      
+  }})
+  }
+
+  function handleCancel(){
+    setProjectsState(
+      prev =>{
+        return{
+          ...prev,
+          selectedProjectId: undefined,
+        }
+      }
+    )
+  }
+
+  
+    function handleAddProject({projectData}){
+        setProjectsState(prev =>{
+          const newProject = {
+            ...projectData,
+            id: Math.random()
+          };
+          return{
+            ...prev,
+            projects: [...prev.projects, newProject]
+          }
+        })
+    }
+
+    console.log(projectsState);
+
+  let content;
+
+  if(projectsState.selectedProjectId === undefined){
+    content =<NoProject onStartAddProject={handleStartAddProject}/>
+  }else if(projectsState.selectedProjectId === null){
+      
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancel}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      {/* <NewProject /> */}
-      <NoProject />
+      <SideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
